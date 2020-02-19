f4e57c10bf3e202579c36046f6b66f886e5ceda5
Jonathan Leahy <jonathanl@newtimesystems.com>
2020-02-19 22:36:20 +0000

    site code tidied

---
+++ src/Components/Footing.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from "react-bootstrap/Navbar";
+import {Col, Row} from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+
+export default function Padding() {
+    return (
+        <>
+            <br/><br/><br/>
+        </>
+    )
+}

3ff6fa12444429090ec64074c522a42acc2e9145
Jonathan Leahy <jonathanl@newtimesystems.com>
2020-02-19 21:23:56 +0000

    bootstrap and react-bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
@@ -16,7 +18,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <Button>Learn React</Button>
         </a>
       </header>
     </div>

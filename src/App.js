e5307a68ac4a9e731799f06d45c56a80122b2000
Jonathan Leahy <jonathanl@newtimesystems.com>
2020-02-19 21:33:21 +0000

    eslint and prettier added. config from https://github.com/facebook/create-react-app

--- src/App.js
+++ src/App.js
@@ -5,24 +5,24 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Button>Learn React</Button>
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <Button>Learn React</Button>
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
